59e7588d85dbe2c88b5cef3dcac6308dc47f2e44
pomidor <Vladimir Elizarov>
2020-01-08 01:33:02 +0300

    add id route

---
+++ src/components/Product.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <my-header></my-header>
+    <h1> This is the id {{ $route.params.id }}</h1>
+    <div class="row">
+      <div class="col-md-5 col-md-offset-0">
+        <figure>
+          <img :src="product.image" class="product">
+        </figure>
+      </div>
+      <div class="col-md-6 col-md-offset-0 description">
+        <h1>{{ product.title }}</h1>
+        <p v-html="product.description"></p>
+        <p class="price">
+          {{ product.price }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyHeader from './Header'
+export default {
+  components: { MyHeader },
+  data() {
+    return {
+      product: ''
+    }
+  },
+  created: function() {
+    axios.get('/static/products.json')
+    .then((response)=> {
+      this.product = response.data.products.filter(
+        data => data.id == this.$route.params.id)[0]
+      this.product.image = '/' + this.product.image      
+    })
+  }
+}
+</script>
